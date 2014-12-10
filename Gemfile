cae40cb4603eeb9f8b8a6768e96446d8adae64d2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-12-10 11:23:24 +0700

    Bump middleman and set deployment settings

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.2.2"
+gem "middleman", "~> 3.3"
 gem "middleman-deploy"
 
 # For faster file watcher updates on Windows:
--- config.rb
+++ config.rb
@@ -6,4 +6,6 @@ set :images_dir, 'images'
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
+  deploy.remote = 'git@github.com:spacetme/spacetme.github.io.git'
+  deploy.branch = 'master'
 end
