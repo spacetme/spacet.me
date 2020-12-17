37b2982d47b5d5b0cef7ba1f669992af7443fe5a
Thai Pangsakulyanont <dtinth@spacet.me>
2020-12-17 16:36:05 +0000

    Remove deployment config from Middleman

--- config.rb
+++ config.rb
@@ -2,10 +2,3 @@
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.method = :git
-  deploy.remote = 'git@github.com:spacetme/spacetme.github.io.git'
-  deploy.branch = 'master'
-end
